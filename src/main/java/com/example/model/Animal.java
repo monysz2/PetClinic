618e08af3734b2191ed11ff441ccd61fd549ce13
SwiderskaA <olaswiderska95@gmail.com>
2018-06-17 13:28:22 +0200

    add enums to Animal entity, starting work in animal view

--- src/main/java/com/example/model/Animal.java
+++ src/main/java/com/example/model/Animal.java
@@ -1,5 +1,9 @@
 package com.example.model;
 
+import com.example.common.enums.AnimalSex;
+import com.example.common.enums.AnimalType;
+import org.hibernate.validator.constraints.NotEmpty;
+
 import javax.persistence.*;
 import java.util.Date;
 
@@ -11,18 +15,25 @@ public class Animal {
     @Column(name="id")
     @Id
     private long id;
+
     @Column(name="name")
+    @NotEmpty(message = "*Please provide name")
     private String name;
     @Column(name="date_of_birth")
     private java.util.Date dateOfBirth;
     @Column(name="sex")
-    private String sex;
+    @Enumerated(EnumType.STRING)
+    private AnimalSex sex;
     @ManyToOne(fetch=FetchType.EAGER)
     @JoinColumn(name="owner_id")
     private User owner;
 
     @Column(name="breed")
     private String breed;
+
+    @Column(name="animal_type")
+    @Enumerated(EnumType.STRING)
+    private AnimalType animalType;
 
     public long getId() {
         return id;
@@ -48,14 +59,6 @@ public class Animal {
         this.dateOfBirth = dateOfBirth;
     }
 
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
     public User getOwner() {
         return owner;
     }
@@ -70,5 +73,21 @@ public class Animal {
 
     public void setBreed(String breed) {
         this.breed = breed;
+    }
+
+    public AnimalSex getSex() {
+        return sex;
+    }
+
+    public void setSex(AnimalSex sex) {
+        this.sex = sex;
+    }
+
+    public AnimalType getAnimalType() {
+        return animalType;
+    }
+
+    public void setAnimalType(AnimalType animalType) {
+        this.animalType = animalType;
     }
 }
