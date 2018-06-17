89b540ed479590b67e58935e7d99877b93ef2d70
SwiderskaA <olaswiderska95@gmail.com>
2018-06-17 18:42:34 +0200

    nice desc for hours

--- src/main/java/com/example/common/enums/WorkingHours.java
+++ src/main/java/com/example/common/enums/WorkingHours.java
@@ -1,21 +1,30 @@
 package com.example.common.enums;
 
 public enum WorkingHours {
-    HOURS_8,
-    HOURS_8_30,
-    HOURS_9,
-    HOURS_9_30,
-    HOURS_10,
-    HOURS_10_30,
-    HOURS_11,
-    HOURS_11_30,
-    HOURS_12,
-    HOURS_12_30,
-    HOURS_13,
-    HOURS_13_30,
-    HOURS_14,
-    HOURS_14_30,
-    HOURS_15,
-    HOURS_15_30
+    HOURS_8("8:00"),
+    HOURS_8_30("8:30"),
+    HOURS_9("9:00"),
+    HOURS_9_30("9:30"),
+    HOURS_10("10:00"),
+    HOURS_10_30("10:30"),
+    HOURS_11("11:00"),
+    HOURS_11_30("11:30"),
+    HOURS_12("12:00"),
+    HOURS_12_30("12:30"),
+    HOURS_13("13:00"),
+    HOURS_13_30("13:30"),
+    HOURS_14("14:00"),
+    HOURS_14_30("14:30"),
+    HOURS_15("15:00"),
+    HOURS_15_30("15:30");
 
+    private String name;
+
+    WorkingHours(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/com/example/controller/admin/VisitController.java
+++ src/main/java/com/example/controller/admin/VisitController.java
@@ -2,6 +2,7 @@ package com.example.controller.admin;
 
 import com.example.common.enums.AnimalSex;
 import com.example.common.enums.AnimalType;
+import com.example.common.enums.WorkingHours;
 import com.example.model.Animal;
 import com.example.model.User;
 import com.example.model.Visit;
@@ -10,12 +11,15 @@ import com.example.service.UserService;
 import com.example.service.VisitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -33,21 +37,18 @@ public class VisitController {
     public ModelAndView listUsers() {
         ModelAndView modelAndView = new ModelAndView();
         Visit visit = new Visit();
+        visit.setDate(new Date());
         List<Animal> pets = animalService.findAll();
         List<Visit> visits = visitService.findAll();
 
         modelAndView.addObject("pets", pets);
         modelAndView.addObject("visit", visit);
         modelAndView.addObject("visits", visits);
+        modelAndView.addObject("hours", WorkingHours.values());
 
         modelAndView.setViewName("admin/visits");
 
         return modelAndView;
-    }
-
-    @RequestMapping(value = "/admin/visits/hours", method = RequestMethod.GET)
-    public String freeHours() {
-        return "test";
     }
 
     @RequestMapping(value = "/admin/visits", method = RequestMethod.POST)
